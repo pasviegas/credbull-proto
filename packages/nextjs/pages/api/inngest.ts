95c8e19b9eb8d0f41f406f25b2aae55f2c22529f
Pedro Viegas <pedroasv@gmail.com>
2023-12-13 17:23:36 -0300

    chore: make backend call more resilient

---
+++ packages/nextjs/pages/api/inngest.ts
@@ -0,0 +1,61 @@
+import prisma from "../../db";
+import { Contract, Wallet, getDefaultProvider } from "ethers";
+import { Inngest } from "inngest";
+import { serve } from "inngest/next";
+import deployedContracts from "~~/contracts/deployedContracts";
+
+const contract = deployedContracts[5].CredbullBadge;
+
+function splitToNChunks(array: any[], n: number) {
+  const result = [];
+  for (let i = n; i > 0; i--) {
+    result.push(array.splice(0, Math.ceil(array.length / i)));
+  }
+  return result;
+}
+
+export const inngest = new Inngest({ id: "credbull-proto" });
+
+const refreshTierFn = inngest.createFunction(
+  { id: "refresh-tier-fn" },
+  { event: "ethers/refresh.tier" },
+  async ({ event, step }) => {
+    const privateKey = process.env.DEPLOYER_PRIVATE_KEY ?? "";
+    const provider = getDefaultProvider("goerli");
+    const signer = new Wallet(privateKey, provider);
+    const badge = new Contract(contract.address, contract.abi, signer) as any;
+
+    const chunks = await step.run("get-chunks", async () => {
+      const points = await prisma.points.findMany();
+      points.sort((a, b) => (b.points ?? 0) - (a.points ?? 0));
+
+      return splitToNChunks(points, 3);
+    });
+
+    await step.run("update-tier-1", async () => {
+      for (const point of chunks[0]) {
+        await badge.setTier(point.address, BigInt(1));
+      }
+    });
+
+    await step.run("update-tier-2", async () => {
+      for (const point of chunks[1]) {
+        await badge.setTier(point.address, BigInt(2));
+      }
+    });
+
+    await step.run("update-tier-3", async () => {
+      for (const point of chunks[2]) {
+        await badge.setTier(point.address, BigInt(3));
+      }
+    });
+
+    return { event, body: "Hello, World!" };
+  },
+);
+
+// Create an API that serves zero functions
+export default serve({
+  client: inngest,
+  functions: [refreshTierFn],
+});
--- packages/nextjs/pages/api/refresh-tier.ts
+++ packages/nextjs/pages/api/refresh-tier.ts
@@ -1,44 +1,12 @@
-import prisma from "../../db";
-import { Contract, Wallet, getDefaultProvider } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
-import deployedContracts from "~~/contracts/deployedContracts";
+import { inngest } from "~~/pages/api/inngest";
 
 type Status = {
   success: boolean;
 };
 
-function splitToNChunks(array: any[], n: number) {
-  const result = [];
-  for (let i = n; i > 0; i--) {
-    result.push(array.splice(0, Math.ceil(array.length / i)));
-  }
-  return result;
-}
-
-const contract = deployedContracts[5].CredbullBadge;
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Status>) {
-  const points = await prisma.points.findMany();
-  points.sort((a, b) => (b.points ?? 0) - (a.points ?? 0));
-
-  const chunks = splitToNChunks(points, 3);
-
-  const privateKey = process.env.DEPLOYER_PRIVATE_KEY ?? "";
-  const provider = getDefaultProvider("goerli");
-  const signer = new Wallet(privateKey, provider);
-  const badge = new Contract(contract.address, contract.abi, signer);
-
-  for (const point of chunks[0]) {
-    await badge.setTier(point.address, BigInt(1));
-  }
-
-  for (const point of chunks[1]) {
-    await badge.setTier(point.address, BigInt(2));
-  }
-
-  for (const point of chunks[2]) {
-    await badge.setTier(point.address, BigInt(3));
-  }
+  await inngest.send({ name: "ethers/refresh.tier", data: {} });
 
   res.status(200).json({ success: true });
 }
