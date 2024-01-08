23a26be281f90f4be55d3b577648a6df5a5beb09
Pedro Viegas <pedroasv@gmail.com>
2024-01-08 07:02:34 -0300

    feat: add api auth

--- apps/api/src/clients/supabase/supabase.service.ts
+++ apps/api/src/clients/supabase/supabase.service.ts
@@ -1,4 +1,3 @@
-import { Database } from '@credbull/supabase';
 import { Inject, Injectable, Scope } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { REQUEST } from '@nestjs/core';
@@ -6,30 +5,51 @@ import { SupabaseClient, createClient } from '@supabase/supabase-js';
 import { Request } from 'express';
 import { ExtractJwt } from 'passport-jwt';
 
+import { Database } from '../../types/supabase';
+
 @Injectable({ scope: Scope.REQUEST })
 export class SupabaseService {
   private supabase: SupabaseClient<Database>;
+  private supabaseAdmin: SupabaseClient<Database>;
 
   constructor(
     private readonly config: ConfigService,
     @Inject(REQUEST) private readonly request: Request,
   ) {}
 
+  static createClientFromToken(publicUrl: string, privateKey: string, token: string | null) {
+    return createClient<Database, 'public'>(publicUrl, privateKey, {
+      auth: { persistSession: false },
+      global: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    });
+  }
+
   client() {
     if (this.supabase) return this.supabase;
 
-    this.supabase = createClient<Database, 'public'>(
+    this.supabase = SupabaseService.createClientFromToken(
       this.config.getOrThrow('NEXT_PUBLIC_SUPABASE_URL'),
       this.config.getOrThrow('NEXT_PUBLIC_SUPABASE_ANON_KEY'),
+      ExtractJwt.fromAuthHeaderAsBearerToken()(this.request),
+    );
+
+    return this.supabase;
+  }
+
+  admin() {
+    if (this.supabaseAdmin) return this.supabaseAdmin;
+
+    this.supabaseAdmin = createClient<Database, 'public'>(
+      this.config.getOrThrow('NEXT_PUBLIC_SUPABASE_URL'),
+      this.config.getOrThrow('SUPABASE_SERVICE_ROLE_KEY'),
       {
         auth: { persistSession: false },
-        global: {
-          headers: {
-            Authorization: `Bearer ${ExtractJwt.fromAuthHeaderAsBearerToken()(this.request)}`,
-          },
-        },
       },
     );
-    return this.supabase;
+    return this.supabaseAdmin;
   }
 }
