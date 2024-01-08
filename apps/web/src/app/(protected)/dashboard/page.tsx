d343274461ec29355a4e44d299911998df4825c1
Pedro Viegas <pedroasv@gmail.com>
2024-01-08 07:07:16 -0300

    feat: add refresh token to frontend

--- apps/web/src/app/(protected)/dashboard/page.tsx
+++ apps/web/src/app/(protected)/dashboard/page.tsx
@@ -6,13 +6,13 @@ import { accountStatus } from '@/app/(protected)/dashboard/account-status.action
 
 export default async function Dashboard() {
   const supabase = createClient(cookies());
-  const { data: auth } = await supabase.auth.getUser();
+  const { data: auth } = await supabase.auth.getSession();
 
   const account = await accountStatus();
 
   return (
     <div>
-      Hey, {auth.user?.email}! You are {account.status}
+      Hey, {auth.session?.user?.email}! You are {account.status}. Your REFRESH TOKEN is {auth.session?.refresh_token}.
     </div>
   );
 }
